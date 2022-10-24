1014151292d8e1e2bab4dcd899e7c4a38f9223db
Shun <saitoshin2145@gmail.com>
2022-10-24 14:44:29 +0900

    issue-62: fix landing page layout

--- app/imports/ui/components/SideNavBar/DesktopSideBar.jsx
+++ app/imports/ui/components/SideNavBar/DesktopSideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Nav, Button } from 'react-bootstrap';
@@ -11,11 +11,29 @@ import MobileSignUpModal from '../MobileSignUpModal';
 import MobileSignOutCheck from '../MobileSignOutCheck';
 
 const DesktopSideBar = () => {
+  // the width of the screen using React useEffect
+  const [width, setWidth] = useState(window.innerWidth);
+  // make sure that it changes with the window size
+  useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    // subscribe to window resize event "onComponentDidMount"
+    window.addEventListener('resize', handleResizeWindow);
+    return () => {
+      // unsubscribe "onComponentDestroy"
+      window.removeEventListener('resize', handleResizeWindow);
+    };
+  }, []);
+
+  const minWidth = 0.1 * width;
+  const maxWidth = 0.3 * width;
+  const closeminWidth = 0.1 * width;
+  const closemaxWidth = 0.12 * width;
+
   const showSideBarStyle = {
     position: 'fixed',
     minHeight: '100vh',
-    maxWidth: '15%',
-    minWidth: '20%',
+    maxWidth: maxWidth,
+    minWidth: minWidth,
     backgroundColor: 'cyan',
     color: 'white',
     fontSize: '16px',
@@ -25,14 +43,13 @@ const DesktopSideBar = () => {
 
   const closeSideBarStyle = {
     position: 'fixed',
-    maxWidth: '15%',
-    minWidth: '10%',
-
+    maxWidth: closemaxWidth,
+    minWidth: closeminWidth,
     minHeight: '100vh',
     backgroundColor: 'cyan',
     color: 'white',
     fontSize: '16px',
-    textAlign: 'center',
+    textAlign: 'left',
     zIndex: 10,
   };
 
@@ -52,7 +69,7 @@ const DesktopSideBar = () => {
   return (
     <div>
       {show ? (
-        <Nav style={showSideBarStyle} activeKey="/home">
+        <Nav style={showSideBarStyle} activeKey='/home'>
           <Nav.Item>
             <Button
               style={{
@@ -60,31 +77,30 @@ const DesktopSideBar = () => {
                 color: 'white',
                 fontSize: '20px',
               }}
-              onClick={handleClose}
-            >
+              onClick={handleClose}>
               <Icon.ArrowBarLeft />
             </Button>
-            <Nav.Link href="/" style={{ color: 'black' }}>
+            <Nav.Link href='/' style={{ color: 'black' }}>
               <Icon.HouseDoor /> <b>HOME</b>
             </Nav.Link>
-            <Nav.Link href="/all-dashboard" style={{ color: 'black' }}>
+            <Nav.Link href='/all-dashboard' style={{ color: 'black' }}>
               <Icon.Clipboard /> <b>DASHBOARD</b>
             </Nav.Link>
 
             {currentUser
               ? [
-                <>
-                  <Nav.Link href="/home" style={{ color: 'black' }}>
-                    <Icon.Inbox /> <b>NOTIFICATION</b>
-                  </Nav.Link>
-                  <Nav.Link href="/calendar" style={{ color: 'black' }}>
-                    <Icon.Calendar /> <b>CALENDAR</b>
-                  </Nav.Link>
-                </>,
-              ]
+                  <>
+                    <Nav.Link href='/home' style={{ color: 'black' }}>
+                      <Icon.Inbox /> <b>NOTIFICATION</b>
+                    </Nav.Link>
+                    <Nav.Link href='/calendar' style={{ color: 'black' }}>
+                      <Icon.Calendar /> <b>CALENDAR</b>
+                    </Nav.Link>
+                  </>,
+                ]
               : ''}
             {currentUser === '' ? (
-              <div style={{ textAlign: 'center', lineHeight: '45pt' }}>
+              <div style={{ lineHeight: '45pt' }}>
                 <SignInModal />
                 <br />
                 <SignUpModal />
@@ -98,35 +114,33 @@ const DesktopSideBar = () => {
           </Nav.Item>
         </Nav>
       ) : (
-        <Nav style={closeSideBarStyle} activeKey="/home">
+        <Nav style={closeSideBarStyle} activeKey='/home'>
           <Nav.Item>
             <Button
               style={{
-                textAlign: 'center',
                 color: 'white',
                 fontSize: '20px',
               }}
-              onClick={handleShow}
-            >
+              onClick={handleShow}>
               <Icon.List />
             </Button>
-            <Nav.Link href="/" style={{ color: 'black' }}>
+            <Nav.Link href='/' style={{ color: 'black' }}>
               <Icon.HouseDoor />
             </Nav.Link>
-            <Nav.Link href="/all-dashboard" style={{ color: 'black' }}>
+            <Nav.Link href='/all-dashboard' style={{ color: 'black' }}>
               <Icon.Clipboard />
             </Nav.Link>
             {currentUser
               ? [
-                <>
-                  <Nav.Link href="/home" style={{ color: 'black' }}>
-                    <Icon.Inbox />
-                  </Nav.Link>
-                  <Nav.Link href="/calendar" style={{ color: 'black' }}>
-                    <Icon.Calendar />
-                  </Nav.Link>
-                </>,
-              ]
+                  <>
+                    <Nav.Link href='/home' style={{ color: 'black' }}>
+                      <Icon.Inbox />
+                    </Nav.Link>
+                    <Nav.Link href='/calendar' style={{ color: 'black' }}>
+                      <Icon.Calendar />
+                    </Nav.Link>
+                  </>,
+                ]
               : ''}
             {currentUser === '' ? (
               <div style={{ textAlign: 'center', lineHeight: '45pt' }}>
