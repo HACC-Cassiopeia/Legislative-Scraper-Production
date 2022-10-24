a5fca771c834aacb5b8f0b238508d9c8ceaf1471
Giorgio <tran.giorgio@gmail.com>
2022-10-23 21:10:03 -1000

    Delete stuff

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
 import { Col, Image, Row, Container } from 'react-bootstrap';
 
 // added
@@ -35,7 +36,7 @@ const messageStyle = {
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <div>
-    <div className='headerTopTitle'>
+    <div className="headerTopTitle">
       <div style={centerBreak} />
       <Container fluid style={introMessageStyle}>
         <h3 style={{ top: '50%' }}>Track & Monitor your proposals</h3>
@@ -52,10 +53,10 @@ const Landing = () => (
         <p>See what kind of ideas are being proposed.</p>
       </Col>
       <Col>
-        <Image
-          style={{ maxWidth: '45%' }}
-          src='https://1.bp.blogspot.com/-C6vAsZfl8ic/Xq5vGe5oFpI/AAAAAAABYtE/DPvun2JQUYI7iLNp__Q2zZX5gitobZwyACEwYBhgL/s1600/computer_document_spreadsheet.png'
-        />
+        {/* <Image */}
+        {/*  style={{ maxWidth: '45%' }} */}
+        {/*  src='https://1.bp.blogspot.com/-C6vAsZfl8ic/Xq5vGe5oFpI/AAAAAAABYtE/DPvun2JQUYI7iLNp__Q2zZX5gitobZwyACEwYBhgL/s1600/computer_document_spreadsheet.png' */}
+        {/* /> */}
       </Col>
     </Row>
     <Row style={landingStyleB}>
@@ -67,10 +68,10 @@ const Landing = () => (
       </Col>
 
       <Col>
-        <Image
-          style={{ maxWidth: '35%' }}
-          src='https://4.bp.blogspot.com/-n39YthHIJDY/Wn1VskGy9MI/AAAAAAABKEU/600dCV5x4uINvIyB_1a2xI82mxzA5kOZACLcBGAs/s350/computer_businessman1_smile.png'
-        />
+        {/* <Image */}
+        {/*  style={{ maxWidth: '35%' }} */}
+        {/*  src='https://4.bp.blogspot.com/-n39YthHIJDY/Wn1VskGy9MI/AAAAAAABKEU/600dCV5x4uINvIyB_1a2xI82mxzA5kOZACLcBGAs/s350/computer_businessman1_smile.png' */}
+        {/* /> */}
       </Col>
     </Row>
     <div style={messageStyle}>
