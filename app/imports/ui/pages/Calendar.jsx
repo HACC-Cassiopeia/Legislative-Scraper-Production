7419bf70b1e84d23722f41e01175e8065bd6e625
Giorgio <tran.giorgio@gmail.com>
2022-10-25 14:49:05 -1000

    Populate data of upcoming hearings to calendar

--- app/imports/ui/pages/Calendar.jsx
+++ app/imports/ui/pages/Calendar.jsx
@@ -3,33 +3,11 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { Container } from 'react-bootstrap';
 import Legtracker from '../utilities/Legtracker';
-// added
-function getDate(dayString) {
-  const today = new Date();
-  const year = today.getFullYear().toString();
-  let month = (today.getMonth() + 1).toString();
-
-  if (month.length === 1) {
-    month = `0${month}`;
-  }
-
-  return dayString.replace('YEAR', year).replace('MONTH', month);
-}
-
-const events = [
-  {
-    title: 'Meeting',
-    start: getDate('YEAR-MONTH-11T10:30:00+00:00'),
-    end: getDate('YEAR-MONTH-11T12:30:00+00:00'),
-  },
-  { title: 'Happy Hour', start: getDate('YEAR-MONTH-13T17:30:00+00:00') },
-  { title: 'Dinner', start: getDate('YEAR-MONTH-18T20:00:00+00:00') },
-];
 
 const Calendar = () => {
   const [upcomingHearings, setUpcomingHearings] = useState([]);
   useEffect(() => {
-    // document.title = 'DOE Legislative Tracker - Calendar';
+    document.title = 'DOE Legislative Tracker - Calendar';
     Legtracker
       .scrapeUpcomingHearings()
       .then(initialData => {
@@ -49,7 +27,12 @@ const Calendar = () => {
             right: 'dayGridMonth,timeGridWeek,timeGridDay',
           }}
           plugins={[dayGridPlugin]}
-          events={events}
+          events={upcomingHearings.map(data => (
+            {
+              title: data.measure,
+              start: data.dateTime,
+            }
+          ))}
         />
       </Container>
     </div>
